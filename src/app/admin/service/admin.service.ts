35d9693e3f806c399ac94c7a58fada4fb78a3a19
youth1506 <shivampabrekar123@gmail.com>
2024-08-04 22:20:03 +0530

    created analytics page

--- src/app/admin/service/admin.service.ts
+++ src/app/admin/service/admin.service.ts
@@ -90,6 +90,12 @@ addCategory(categoryDto:any): Observable<any>{
     })
   }
 
+  getAnalytics(): Observable<any>{
+    return this.http.get(BASIC_URL + `api/admin/order/analytics`, {
+      headers: this.createAuthorizationHeader(),
+    })
+  }
+
   private createAuthorizationHeader(): HttpHeaders{
     return new HttpHeaders().set(
       'Authorization', 'Bearer ' + UserStorageService.getToken()
